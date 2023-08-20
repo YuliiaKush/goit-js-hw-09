38f86d010bbc711da95a9ead770b4b6ae4563055
YuliiaKush <128642386+YuliiaKush@users.noreply.github.com>
2023-08-20 23:03:18 +0300

    final update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,65 +1,80 @@
 import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
+import 'flatpickr/dist/flatpickr.min.css';
 
-let currentTime = new Date(); 
-let selectedTime; 
-const refs = {
-  inputDate: document.querySelector('#datetime-picker'),
-  days: document.querySelector('span[data-days]'),
-  hours: document.querySelector('span[data-hours]'),
-  minutes: document.querySelector('span[data-minutes]'),
-  seconds: document.querySelector('span[data-seconds]'),
-  btn: document.querySelector('button[data-start]'), 
-};
-refs.btn.disabled = true; 
-const pickedDate = flatpickr(refs.inputDate, {
+const daysEl = document.querySelector('.value[data-days]');
+const hoursEl = document.querySelector('.value[data-hours]');
+const minutesEl = document.querySelector('.value[data-minutes]');
+const secondsEl = document.querySelector('.value[data-seconds]');
+const startBtn = document.querySelector('button[data-start]');
+const myInput = document.querySelector('#datetime-picker');
+
+let timerIsStarted = false;
+startBtn.disabled = true;
+
+const fp = flatpickr(myInput, {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    const dateDiff = selectedDates[0].getTime() - new Date().getTime();
+    if (dateDiff > 0 && !timerIsStarted) {
+      startBtn.disabled = false;
 
-      if (selectedDates[0] <= currentTime) {
-          Notiflix.Report.warning(
-                "Please choose a date in the future"
-            );
+      startBtn.addEventListener('click', () => {
+        startTimer(dateDiff);
+        timerIsStarted = true;
+        startBtn.disabled = true;
+      });
     } else {
-      refs.btn.disabled = false;
+      startBtn.disabled = true;
+      if (timerIsStarted) {
+        Notiflix.Notify.failure('Timer is started');
+      } else {
+        Notiflix.Notify.failure('Please choose the future time');
+      }
     }
-    selectedTime = selectedDates[0];
   },
 });
 
-refs.btn.addEventListener('click', () => {
-  function pad(value) {
-    return String(value).padStart(2, '0');
-  }
-  let updateTimer = setInterval(() => {
-    let currentTime = new Date();
-    const newTimer = selectedTime - currentTime;
-    const { days, hours, minutes, seconds } = convertMs(newTimer);
-    refs.days.textContent = days;
-    refs.hours.textContent = hours;
-    refs.minutes.textContent = minutes;
-    refs.seconds.textContent = seconds;
+function startTimer(milliseconds) {
+  let timerId = setInterval(() => {
+    if (milliseconds > 0) {
+      printTime(convertMs(milliseconds));
+      milliseconds -= 1000;
+    } else {
+      clearInterval(timerId);
+    }
   }, 1000);
+}
 
-  function convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-    // Remaining days
-    const days = pad(Math.floor(ms / day));
-    // Remaining hours
-    const hours = pad(Math.floor((ms % day) / hour));
-    // Remaining minutes
-    const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-    // Remaining seconds
-    const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
-    return { days, hours, minutes, seconds };
-  }
-});
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function printTime(t) {
+  daysEl.textContent = addLeadingZero(t.days);
+  hoursEl.textContent = addLeadingZero(t.hours);
+  minutesEl.textContent = addLeadingZero(t.minutes);
+  secondsEl.textContent = addLeadingZero(t.seconds);
+}
